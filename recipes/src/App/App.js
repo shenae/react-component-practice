9c279e43c477bdc523e8961016bface7fd5163ed
Shenae Simmons <shhnae@gmail.com>
2019-04-20 13:43:13 -0400

    final draft: edited and styled components

--- recipes/src/App/App.js
+++ recipes/src/App/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import './style.css';
-
 import recipes from '../data/recipes';
+import RecipePreparation from '../components/RecipePreparation/RecipePreparation.js';
+import Recipe from '../components/Recipe/Recipe.js';
 
 const recipe = recipes[0];
 
 class App extends Component {
+
+  prep = () => {
+    const recipePrep = recipes.preparation[0];
+      console.log(recipePrep);
+      return recipePrep;
+ }
   render() {
     return (
-      <div className="flex-container"></div>
+      <div>
+      <div class="App">
+      <div class="flex-container">
+      <nav class="nav">
+        <button class="btn btn-default">Previous Recipe</button>
+        <button class="btn btn-default">Next Recipe</button>
+      </nav>
+      <header>
+        <h1>CHICKEN TIKKA MASALA!</h1>
+        <cite class="contributors">
+              <div>By John Doe</div>
+              <div><a href="https://cooking.nytimes.com/recipes/5593-chicken-and-apricot-masala" target="_blank">Source</a></div>
+              </cite>
+        <ul id="recipe_meta_data" class="list-group">
+          <li class="list-group-item">Active: 1 Hour</li>
+          <li class="list-group-item">Total: 5 Hours</li>
+          <li class="list-group-item">6 Servings</li>
+        </ul>
+      </header>
+      <article>
+      <Recipe />
+      <RecipePreparation />
+      </article>
+      </div>
+      </div>
+      </div>
     );
   }
 }
---
+++ recipes/src/components/RecipePreparation/RecipePreparation.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class RecipePreparation extends Component {
+
+//   prep = () => {
+//         const recipePrep = recipes.preparation[0];
+//           console.log(recipePrep);
+//           return recipePrep;
+//      }
+
+render() {
+    return (
+      <div>
+      <h1>Preparation</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    
+      <footer>Copyright &copy; General Assembly</footer>
+    </div>  
+    );
+ }
+}
+export default RecipePreparation;
